99181c0fe1ebeceb6c15d9d7b488c00035ad01f5
wangsongiam <wangsongiam@gmail.com>
2019-03-11 15:04:02 -0700

    added linter

--- src/index.jsx
+++ src/index.jsx
@@ -1,54 +1,64 @@
 import React, { useEffect, useState } from 'react';
-import { timer, from } from 'rxjs';
+import PropTypes from 'prop-types';
+import { timer, from, of } from 'rxjs';
 import { map, catchError, switchMap, timeout, pairwise } from 'rxjs/operators';
-import isReachable from 'is-reachable'
+import isReachable from 'is-reachable';
+
 
 import './index.css';
 
 
-const timer$ = timer(0, 3000)
+const timer$ = timer(0, 3000);
 const getCheckReachable$ = url => from(isReachable(url)).pipe(
   timeout(2500),
   catchError(() => of(false)),
-)
+);
 
 const poll$ = timer$.pipe(
   switchMap(() => getCheckReachable$('google.com')),
-)
+);
 
 const classNameMapper = ([last, curr]) => {
-  if (!last && curr) return 'banner banner-reconnect'
-  if (last && curr) return 'banner'
-  return 'banner banner-disconnect'
-}
+  if (!last && curr) return 'banner banner-reconnect';
+  if (last && curr) return 'banner';
+  return 'banner banner-disconnect';
+};
 
 const className$ = poll$.pipe(
   pairwise(),
   map(classNameMapper),
-)
+);
 
-export default ({ children }) => {
-  const [msg, setMsg] = useState('')
-  const [className, setClassName] = useState('banner')
+const Main = ({ children }) => {
+  const [msg, setMsg] = useState('');
+  const [className, setClassName] = useState('banner');
 
   const observer = n => {
-    setClassName(n)
-    if (n === 'banner banner-reconnect') setMsg('reconnected')
-    else if (n === 'banner banner-disconnect') setMsg('disconnected')
-    else setMsg('')
-  }
+    setClassName(n);
+    if (n === 'banner banner-reconnect') setMsg('reconnected');
+    else if (n === 'banner banner-disconnect') setMsg('disconnected');
+    else setMsg('');
+  };
 
   useEffect(() => {
-    const classNameSub = className$.subscribe(observer)
+    const classNameSub = className$.subscribe(observer);
     return () => {
-      classNameSub.unsubscribe()
-    }
-  })
+      classNameSub.unsubscribe();
+    };
+  });
 
   return (
     <>
       <div className={className} data-conn={msg}></div>
       {children}
     </>
-  )
-}
+  );
+};
+
+
+Main.propTypes = {
+  children: PropTypes.element
+};
+
+
+export default Main;
