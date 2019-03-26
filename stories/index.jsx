e986106e2c92dbab8fa5e89a2fd2b97ec867b999
wangsongiam <wangsongiam@gmail.com>
2019-03-25 22:44:51 -0700

    added grid layout example

--- stories/index.jsx
+++ stories/index.jsx
@@ -3,6 +3,10 @@ import { storiesOf } from '@storybook/react';
 
 import Connectivity from '../src/index.jsx';
 
+import GridLayout from 'react-grid-layout';
+import 'react-grid-layout/css/styles.css';
+import 'react-resizable/css/styles.css';
+
 import './index.css';
 
 
@@ -35,3 +39,36 @@ storiesOf('offline', module)
       <h1>I am pointing to a unconnectable endpoint</h1>
     </div>
   ));
+
+
+
+storiesOf('grid', module)
+  .add('with basic grid layout', () => {
+    let layout = [
+      {i: 'a', x: 0, y: 0, w: 3, h: 2, static: true},
+      {i: 'b', x: 3, y: 0, w: 3, h: 2, minW: 2, maxW: 4},
+      {i: 'c', x: 0, y: 0, w: 5, h: 2}
+    ];
+
+    const endpoints = [
+      'localhost',
+      'google.com',
+      'non-existing-endpoint.com'
+    ];
+
+    layout.map((e, i) => {
+      e.i = endpoints[i]
+      return e
+    })
+
+    return (
+      <GridLayout className="layout" layout={layout} cols={12} rowHeight={30} width={800} compactType="horizontal">
+        {layout.map((e) => 
+          <div key={e.i}>
+            <Connectivity url={e.i}></Connectivity>
+            <span className="text">{e.i}</span>
+          </div>)
+        }
+      </GridLayout>
+    );
+  });
