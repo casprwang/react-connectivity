09124ff262617ee8e8842069251e33afa40e1da1
wangsongiam <wangsongiam@gmail.com>
2019-03-25 22:47:07 -0700

    test props

--- test/index.test.js
+++ test/index.test.js
@@ -13,8 +13,13 @@ describe('tes online', () => {
   test('should hide online', () => {
     expect(mount(<Connectivity />).find('.banner').length).toBe(1);
   });
-
   test('should render', () => {
     expect(mount(<Connectivity />).find('.banner .banner-disconnect').length).toBe(0);
+  });
+  test('should url props render', () => {
+    expect(mount(<Connectivity url="test-endpoint" />).find('.banner .banner-disconnect').length).toBe(0);
+  });
+  test('should interval render', () => {
+    expect(mount(<Connectivity url="test-endpoint" interval={1000} />).find('.banner .banner-disconnect').length).toBe(0);
   });
 });
